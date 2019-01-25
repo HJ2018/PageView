c28283f001a2edb7418d675e3d904e9e8cd23958
H_vv <597079127@qq.com>
2019-01-25 13:44:25 +0800

    添加宏定义

--- HJPageView/ViewController.swift
+++ HJPageView/ViewController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+
+let kScreenWidth = UIScreen.main.bounds.size.width
+let kScreenHeight = UIScreen.main.bounds.size.height
+
+let isPhone = Bool(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.phone)
+
+let isPhoneX = Bool(kScreenWidth >= 375.0 && kScreenHeight >= 812.0 && isPhone)
+
+let kNavigationHeight = CGFloat(isPhoneX ? 88 : 64)
+
+let kStatusBarHeight = CGFloat(isPhoneX ? 44 : 20)
+
+let kTabBarHeight = CGFloat(isPhoneX ? (49 + 34) : 49)
+
+let kBottomSafeHeight = CGFloat(isPhoneX ? 34 : 0)
+
+let kTopSafeHeight = CGFloat(isPhoneX ? 44 : 0)
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -31,7 +50,7 @@ class ViewController: UIViewController {
             childVcs .append(vc)
         }
         
-        let pagFrame = CGRect(x: 0, y: 64, width: view.bounds.width, height: view.bounds.height - 64)
+        let pagFrame = CGRect(x: 0, y: kNavigationHeight, width: view.bounds.width, height: view.bounds.height - 64)
         
         
         let HJPage = PageView(frame: pagFrame, titles: title, childVcs: childVcs, parentVc: self, style : style)
